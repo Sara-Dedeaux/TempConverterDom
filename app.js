4b9bef44b1e9ff370f70825b82b81f611f6eaf20
Sara-Dedeaux <sara.dedeaux@mscoding.org>
2024-08-26 14:52:51 -0500

    input box and submit button working
    user can input text and submit information

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-//TARGET THE INPUT BOX FOR TEMP VALUE
+// CREATE VARIABLE - TO TARGET ELEMENT FOR TEMP VALUE
+let tempValue= document.getElementById("tempVal")
+console.log(tempValue)
+
+//CREATE VARIABLE TO HOLD USERINPUT 
+let startTempValue= "";
+console.log(startTempValue)
+
+//CREATE VARIABLE FOR EVENT LISTENER
+let enterTempValue= document.getElementById("submitVal")
+console.log(enterTempValue)
+
+//CREATE EVENT LISTENER FOR SUBMIT BUTTON
+enterTempValue.addEventListener("click", () => {
+     
+    startTempValue=tempValue.value;
+    console.log(startTempValue)
+
+})
 
